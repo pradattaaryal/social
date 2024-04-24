4c89a3ec7314b09d51fadf433baf0ae0e9f642e9
pradattaaryal <pradattaaryal2468@gmail.com>
2024-04-24 12:59:09 +0545

    first commit

--- frontend/src/components/Ads/Ads.jsx
+++ frontend/src/components/Ads/Ads.jsx
@@ -16,8 +16,8 @@ export default function Ads() {
           src="https://images.unsplash.com/photo-1518734549841-b417d28c22aa?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           width="100%"
         />
-      </CardContent>
+      </CardContent> s
     </Card>
-  )
+  ) 
 }
 
