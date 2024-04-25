086089c8246001f80e2abd77dccb4c8aad723534
pradattaaryal <128461960+pradattaaryal@users.noreply.github.com>
2024-04-25 15:57:45 +0545

    Update hooks.js

--- frontend/src/lib/hooks.js
+++ frontend/src/lib/hooks.js
@@ -1,23 +1,62 @@
 import { create } from 'zustand';
+import { useEffect } from 'react';
 
 // Define your store
-const useStore = create((set) => ({
-  userData: null,
-  token: null,
-  posts: null,
-  savedpost: null,
-  profiledata: null,
-  setUserDataAndToken: (userData, token) => set({ userData, token }),
-  setpost: (fetcheddata) => set({ posts: fetcheddata }),
-  setsavedpost: (fetcheddata) => set({ savedpost: fetcheddata }),
-  setfriends: (friendId) =>
-    set((state) => ({
-      userData: {
-        ...state.userData,
-        friends: friendId,
-      },
-    })),
-  setprofiledata: (data) => set({ profiledata: data }),
-}));
+const useStore = create((set) => {
+  const storedUserData = JSON.parse(localStorage.getItem('userData'));
+  const storedToken = localStorage.getItem('token');
+  const storedPosts = JSON.parse(localStorage.getItem('posts'));
+  const storedSavedPost = JSON.parse(localStorage.getItem('savedpost'));
+  const storedProfileData = JSON.parse(localStorage.getItem('profiledata'));
+
+  return {
+    userData: storedUserData || null,
+    token: storedToken || null,
+    posts: storedPosts || null,
+    savedpost: storedSavedPost || null,
+    profiledata: storedProfileData || null,
+    setUserDataAndToken: (userData, token) => {
+      set({ userData, token });
+      localStorage.setItem('userData', JSON.stringify(userData));
+      localStorage.setItem('token', token);
+    },
+    setpost: (fetcheddata) => {
+      set({ posts: fetcheddata });
+      localStorage.setItem('posts', JSON.stringify(fetcheddata));
+    },
+    setsavedpost: (fetcheddata) => {
+      set({ savedpost: fetcheddata });
+      localStorage.setItem('savedpost', JSON.stringify(fetcheddata));
+    },
+    setfriends: (friendId) =>
+      set((state) => ({
+        userData: {
+          ...state.userData,
+          friends: friendId,
+        },
+      })),
+    setprofiledata: (data) => {
+      set({ profiledata: data });
+      localStorage.setItem('profiledata', JSON.stringify(data));
+    },
+  };
+});
+
+// Save and load data from localStorage
+useEffect(() => {
+  const storedUserData = JSON.parse(localStorage.getItem('userData'));
+  const storedToken = localStorage.getItem('token');
+  const storedPosts = JSON.parse(localStorage.getItem('posts'));
+  const storedSavedPost = JSON.parse(localStorage.getItem('savedpost'));
+  const storedProfileData = JSON.parse(localStorage.getItem('profiledata'));
+
+  useStore.setState({
+    userData: storedUserData || null,
+    token: storedToken || null,
+    posts: storedPosts || null,
+    savedpost: storedSavedPost || null,
+    profiledata: storedProfileData || null,
+  });
+}, []);
 
 export default useStore;
