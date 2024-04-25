89b486ff6cbad76e7324afd8e2ac8f64a882750f
pradattaaryal <128461960+pradattaaryal@users.noreply.github.com>
2024-04-25 15:20:05 +0545

    Update hooks.js

--- frontend/src/lib/hooks.js
+++ frontend/src/lib/hooks.js
@@ -1,28 +1,23 @@
 import { create } from 'zustand';
-import { persist } from 'zustand/middleware';
 
 // Define your store
-const useStore = create(
-  persist(
-    (set) => ({
-      userData:null,
-      token:null,
-      posts:null,
-      savedpost:null,
-      profiledata:null,
-      setUserDataAndToken: (userData, token) => set({ userData, token }),
-      setpost: (fetcheddata) => set({ posts: fetcheddata }),
-      setsavedpost: (fetcheddata) => set({ savedpost: fetcheddata }),
-      setfriends: (friendId) =>
-        set((state) => ({
-          userData: {
-            ...state.userData,
-            friends: friendId,
-          },
-        })),
-      setprofiledata: (data) => set({ profiledata: data }),
-    }),
-    )
-);
+const useStore = create((set) => ({
+  userData: null,
+  token: null,
+  posts: null,
+  savedpost: null,
+  profiledata: null,
+  setUserDataAndToken: (userData, token) => set({ userData, token }),
+  setpost: (fetcheddata) => set({ posts: fetcheddata }),
+  setsavedpost: (fetcheddata) => set({ savedpost: fetcheddata }),
+  setfriends: (friendId) =>
+    set((state) => ({
+      userData: {
+        ...state.userData,
+        friends: friendId,
+      },
+    })),
+  setprofiledata: (data) => set({ profiledata: data }),
+}));
 
 export default useStore;
