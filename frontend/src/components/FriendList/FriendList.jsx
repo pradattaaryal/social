5017448059e93063d21030d1c7d2fd56ef00584e
pradattaaryal <128461960+pradattaaryal@users.noreply.github.com>
2024-04-25 14:04:02 +0545

    Update FriendList.jsx

--- frontend/src/components/FriendList/FriendList.jsx
+++ frontend/src/components/FriendList/FriendList.jsx
@@ -1,22 +1,24 @@
-import React, { useEffect, useState } from "react"; // Import useEffect
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Useravatar from "../ui/Useravatar";
 import useStore from "@/lib/hooks";
-export default function Friends({ userid,list }) {
-   const [frienddata, setFriendData] = useState([]);
-const {userData}=useStore();
+
+export default function Friends({ userid, list }) {
+  const [frienddata, setFriendData] = useState([]);
+  const { userData } = useStore();
+
   const fetchFriends = async () => {
     try {
       const response = await axios.get(`http://localhost:3000/api/friends/${userid}`);
       const friend = response.data;
       setFriendData(friend);
     } catch (error) {
-      // Handle error
+      console.error("Error fetching friends:", error);
     }
   };
 
   useEffect(() => {
-    fetchFriends();  
+    fetchFriends();
   }, [userData.friends]);
 
   return (
@@ -25,11 +27,21 @@ const {userData}=useStore();
         <header className="flex border-b-2 items-center p-4 border-black dark:border-gray-800">
           <h2 className="text-lg font-medium text-center flex-1">Follow list</h2>
         </header>
-        <div className="flex-1 overflow-auto   py-6 grid gap-4">
-           {frienddata && frienddata.map((item, index) => (
-            <Useravatar key={index} auserPicturePath={item.image} flist={list} aname={item.name} />
-            
-          ))}
+        <div className="flex-1 overflow-auto py-6 grid gap-4">
+          {frienddata &&
+            frienddata.map((item, index) => (
+              <React.Fragment key={index}>
+                {userData && (
+                  <Useravatar
+                    auserPicturePath={item.image}
+                    flist={list}
+                    aname={item.name}
+                    friendId={item._id}
+                    id={userData._id}
+                  />
+                )}
+              </React.Fragment>
+            ))}
         </div>
       </div>
     </div>
