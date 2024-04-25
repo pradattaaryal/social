9c0d31aa74b8d327b5772980165d0f59ccad4a8f
pradattaaryal <128461960+pradattaaryal@users.noreply.github.com>
2024-04-25 14:47:26 +0545

    Update FriendList.jsx

--- frontend/src/components/FriendList/FriendList.jsx
+++ frontend/src/components/FriendList/FriendList.jsx
@@ -1,27 +1,23 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react"; // Import useEffect
 import axios from "axios";
 import Useravatar from "../ui/Useravatar";
 import useStore from "@/lib/hooks";
-
-export default function Friends({ userid, list }) {
-  const [frienddata, setFriendData] = useState([]);
-  const { userData } = useStore();
+export default function Friends({ userid,list }) {
+   const [frienddata, setFriendData] = useState([]);
+const {userData}=useStore();
+  const fetchFriends = async () => {
+    try {
+      const response = await axios.get(`https://social-o53m.onrender.com/api/friends/${userid}`);
+      const friend = response.data;
+      setFriendData(friend);
+    } catch (error) {
+      // Handle error
+    }
+  };
 
   useEffect(() => {
-    const fetchFriends = async () => {
-      try {
-        if (userData && userData.friends) { // Ensure userData and its friends property are not null
-          const response = await axios.get(`https://social-o53m.onrender.com/api/friends/${userid}`);
-          const friend = response.data;
-          setFriendData(friend);
-        }
-      } catch (error) {
-        console.error("Error fetching friends:", error);
-      }
-    };
-
-    fetchFriends();
-  }, [userid, userData]); // Add userData as a dependency
+    fetchFriends();  
+  }, [userData.friends]);
 
   return (
     <div className="w-full border-2 border-black rounded-lg dark:border-gray-800">
@@ -29,17 +25,11 @@ export default function Friends({ userid, list }) {
         <header className="flex border-b-2 items-center p-4 border-black dark:border-gray-800">
           <h2 className="text-lg font-medium text-center flex-1">Follow list</h2>
         </header>
-        <div className="flex-1 overflow-auto py-6 grid gap-4">
-          {frienddata &&
-            frienddata.map((item, index) => (
-              <Useravatar
-                key={index}
-                auserPicturePath={item.image}
-                flist={list}
-                aname={item.name}
-                friendId={item._id}
-              />
-            ))}
+        <div className="flex-1 overflow-auto   py-6 grid gap-4">
+           {frienddata && frienddata.map((item, index) => (
+            <Useravatar key={index} auserPicturePath={item.image} flist={list} aname={item.name} />
+            
+          ))}
         </div>
       </div>
     </div>
