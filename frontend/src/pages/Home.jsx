48f5316038ec20a24f9d248c2dfeefaade44a44e
pradattaaryal <128461960+pradattaaryal@users.noreply.github.com>
2024-04-25 13:23:51 +0545

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -45,7 +45,7 @@ const Home = () => {
             />
           ))}
         </ScrollArea> 
-        <div className='gap-5 hidden md:flex md:flex-col w-full max-w-full md:max-w-[25%]'> <Ads /></div>
+        <div className='gap-5 hidden md:flex md:flex-col w-full max-w-full md:max-w-[25%]'><Friends userid={userData ? userData._id : null} list={true} /><Ads /></div>
       </div>
     </>
   );
